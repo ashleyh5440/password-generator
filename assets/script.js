223fd107d3e4828a55e0cb868248757764485672
Ashley <ashleyh5440@gmail.com>
2023-10-07 14:59:06 -0400

    made updates to javascript code

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// had to define/create the function
+function generatePassword () {
+
+}
+
+var numOfCharacters = window.prompt("How many characters do you want your password to include?");
+
+var conditions = window.prompt("OK to use special characters?" , "OK to use numbers?", "Okay to use lowercase letters?", "OK to use uppercase letters?");
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +22,9 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// prompts: how many characters, okay to use special characters, okay to okay to use numbers, okay for lowercase, okay for uppercase
+// last 4 are confirms, true or false, so if conditionals
+
+// function connected to the button
